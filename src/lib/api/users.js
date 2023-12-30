70f43988424e4642dc8af0909a3dcc1195a0ee04
jackal <v@njackal.com>
2023-12-29 23:06:09 -0600

    users lib

---
+++ src/lib/api/users.js
@@ -0,0 +1,20 @@
+import {PUBLIC_API} from "$env/static/public";
+import axios from 'axios'
+
+export const login = async (username, password) => {
+    return axios.post(PUBLIC_API + "/users/login", {
+        username:username,
+        password:password
+    })
+}
+
+export const logout = async () => {
+    return axios.post( PUBLIC_API + "/users/logout")
+}
+
+export const register = async (username, password) => {
+    return axios.post(PUBLIC_API + "/users/register", {
+        username:username,
+        password:password
+    })
+}
