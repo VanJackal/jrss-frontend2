febed9b4c17a3a91f432f382442d506c260e6f26
jackal <v@njackal.com>
2024-04-19 10:20:11 -0500

    uncommitted changes from months ago, need to verify

--- src/routes/AddFeedDialog.svelte
+++ src/routes/AddFeedDialog.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
     import Modal from "$lib/components/Modal.svelte";
+    import {addFeed, getFeedDetails, Feed} from "$lib/api/feeds"
     export let showDialog:boolean;
 
-    let showAll:boolean = true;
+    let showAll:boolean = false;
+    let feed:Feed = new Feed()
+
+    async function handleSubmit() {
+        if (showAll) {
+            await feed.addFeed()
+        } else {
+            await feed.updateDetails()
+            showAll = true;
+        }
+    }
+
+    export function reset() {
+        feed = new Feed()
+        showAll = false;
+    }
 </script>
 
 <style>
@@ -25,24 +41,24 @@
     <div class="container">
         <div class="input-container">
             <label for="url">Feed URL</label>
-            <input id="url">
+            <input id="url" bind:value={feed.feedUrl}>
         </div>
 
         <!-- after get info -->
         {#if showAll}
             <div class="input-container">
                 <label for="title">Feed Name</label>
-                <input id="title">
+                <input id="title" bind:value={feed.feedTitle}>
             </div>
             <div class="input-container">
                 <label for="tag">Short Name</label>
-                <input id="tag">
+                <input id="tag" bind:value={feed.feedShortTitle}>
             </div>
             <label for="desc">Description</label>
-            <textarea id="desc"/>
+            <textarea id="desc" bind:value={feed.feedDescription}/>
         {/if}
 
 
-        <button id="submit">Submit</button>
+        <button id="submit" on:click={handleSubmit}>Submit</button>
     </div>
 </Modal>
